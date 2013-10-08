ed4f29586eff442045b2b7a87bd51a77a5b61289
Jacky <2326220@163.com>
2013-10-08 17:01:07 +0800

    Init

---
+++ AdminMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Localization;
+using Orchard.Security;
+using Orchard.UI.Navigation;
+
+namespace Jacky.Crawler
+{
+    public class AdminMenu:INavigationProvider
+    {
+        public Localizer T { get; set; }
+
+        public string MenuName
+        {
+            get { return "admin"; }
+        }
+
+        public void GetNavigation(NavigationBuilder builder) {
+            builder.Add(T("Crawler"), "10", item => item.Action("Index", "Admin", new {area = "Jacky.Crawler"}).Permission(StandardPermissions.SiteOwner));
+        }
+    }
+}
